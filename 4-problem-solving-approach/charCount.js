25b53de1e84eec2781fc3b860e10cca8d25eb9a2
mina <minamahernicola@outlook.com>
2021-01-18 18:18:41 +0200

    Refactored the charCount problem

--- 4-problem-solving-approach/charCount.js
+++ 4-problem-solving-approach/charCount.js
@@ -1,21 +1,11 @@
 const countChar = (str) => {
   var counterObj = {};
   str = str.replace(/ /g, '').toLowerCase().split('');
-  var chars = [];
 
   for (let i = 0; i < str.length; i++) {
     if (isNaN(str[i])) {
-      chars.push(str[i]);
-    }
-  }
-
-  for (let i = 0; i < chars.length; i++) {
-    if (chars[i] in counterObj) {
-      const key = chars[i];
-      counterObj[key] += 1;
-    } else {
-      const key = chars[i];
-      counterObj[key] = 1;
+      let key = str[i];
+      counterObj[key] = ++counterObj[key] || 1;
     }
   }
 
