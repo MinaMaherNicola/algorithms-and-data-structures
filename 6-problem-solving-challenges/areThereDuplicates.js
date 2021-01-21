b7b75f3a9cc3688db752e799c93f06e54553955b
mina <minamahernicola@outlook.com>
2021-01-21 11:03:45 +0200

    Refactored areThereDuplicates

--- 6-problem-solving-challenges/areThereDuplicates.js
+++ 6-problem-solving-challenges/areThereDuplicates.js
@@ -1,25 +1,17 @@
-function areThereDuplicates() {
-  if (arguments.length < 2) {
-    return false;
-  }
-
+function areThereDuplicates(...args) {
+  args.sort((a, b) => a > b);
   let pointer = 1;
-  for (let i = 0; i < arguments.length; ) {
-    if (arguments[i] === arguments[pointer]) {
+  for (let i = 0; i < args.length; i++) {
+    if (args[i] === args[pointer]) {
       return true;
     } else {
-      if (pointer >= arguments.length - 1) {
-        i++;
-        pointer = i + 1;
-      } else {
-        pointer++;
-      }
+      pointer++;
     }
   }
 
   return false;
 }
 
-console.log(areThereDuplicates(1, 2, 3, 4, 5, 6));
+// console.log(areThereDuplicates(1, 2, 3, 4, 5, 6));
 // areThereDuplicates(1, 2, 3);
-// areThereDuplicates(1, '2', 'a');
+console.log(areThereDuplicates('a', '2', 'a'));
