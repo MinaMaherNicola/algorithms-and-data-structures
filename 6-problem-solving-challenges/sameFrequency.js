eaddc6c43b171bd4169bd403bf191a6ce0c519bb
mina <minamahernicola@outlook.com>
2021-01-21 10:33:37 +0200

    Solved sameFrequency problem

---
+++ 6-problem-solving-challenges/sameFrequency.js
@@ -0,0 +1,35 @@
+function sameFrequency(int1, int2) {
+  int1 = int1.toString();
+  int2 = int2.toString();
+
+  if (int1.length !== int2.length) {
+    return false;
+  }
+
+  let obj1 = {};
+  let obj2 = {};
+
+  for (let i = 0; i < int1.length; i++) {
+    if (obj1[int1[i]]) {
+      obj1[int1[i]] += 1;
+    } else {
+      obj1[int1[i]] = 1;
+    }
+  }
+
+  for (let i = 0; i < int2.length; i++) {
+    if (obj2[int2[i]]) {
+      obj2[int2[i]] += 1;
+    } else {
+      obj2[int2[i]] = 1;
+    }
+  }
+
+  if (JSON.stringify(obj1) === JSON.stringify(obj2)) {
+    return true;
+  }
+
+  return false;
+}
+
+console.log(sameFrequency(34, 14));
