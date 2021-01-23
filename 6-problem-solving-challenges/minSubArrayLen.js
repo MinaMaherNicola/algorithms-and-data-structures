f8380e75be598c1575bb1a8ec023a6df501334d3
mina <minamahernicola@outlook.com>
2021-01-23 16:20:04 +0200

    Solved minSubArrayLen problem

--- 6-problem-solving-challenges/minSubArrayLen.js
+++ 6-problem-solving-challenges/minSubArrayLen.js
@@ -1,26 +1,35 @@
 const minSubArrayLen = (arr, finalSum) => {
-  arr = arr.sort((a, b) => a > b);
-  let right = arr.length - 1;
-  let left = right - 1;
-  let sum = arr[right];
-  while (left >= 0) {
+  let right = 0;
+  let left = 0;
+  let sum = 0;
+  let minLength = +Infinity;
+
+  while (right < arr.length) {
+    sum += arr[right];
     if (sum >= finalSum) {
-      return right - left;
+      if (minLength > right - left + 1) {
+        minLength = right - left + 1;
+      }
+      left++;
+      right = left + 1;
+      sum = 0;
     } else {
-      sum += arr[left];
-      left--;
+      right++;
     }
   }
-  return 0;
+  if (minLength === Infinity) {
+    return 0;
+  }
+  return minLength - 1;
 };
 
-// console.log(minSubArrayLen([2, 3, 1, 2, 4, 3], 7)); // 2
-// console.log(minSubArrayLen([2, 1, 6, 5, 4], 9)); // 2
-// console.log(minSubArrayLen([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52)); // 1
-// console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 39)); // 3
-// console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 55)); // 5
-// console.log(minSubArrayLen([4, 3, 3, 8, 1, 2, 3], 11)); // 2
-// console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 95)); // 0
+console.log(minSubArrayLen([2, 3, 1, 2, 4, 3], 7)); // 2
+console.log(minSubArrayLen([2, 1, 6, 5, 4], 9)); // 2
+console.log(minSubArrayLen([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52)); // 1
+console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 39)); // 3
+console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 55)); // 5
+console.log(minSubArrayLen([4, 3, 3, 8, 1, 2, 3], 11)); // 2
+console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 95)); // 0
 
 // MY TESTS
-console.log(minSubArrayLen([1, 2, 3, 5, 6, 10, 11, 22, 30, 40], 1));
+// console.log(minSubArrayLen([1, 2, 3, 5, 6, 10, 11, 22, 30, 40], 1));
