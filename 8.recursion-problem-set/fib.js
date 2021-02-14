d3682e17bb2c2aa5ba146a06b050cc547c3230d3
MinaMaherNicola <minamahernicola@outlook.com>
2021-02-14 17:56:32 +0200

    Solved fib recursion

--- 8.recursion-problem-set/fib.js
+++ 8.recursion-problem-set/fib.js
@@ -18,6 +18,11 @@ const fib = (num) => {
   return res;
 };
 
+function fibRec(n){
+  if (n <= 2) return 1;
+  return fib(n-1) + fib(n-2);
+}
+
 console.log(fib(4));
 console.log(fib(10));
 console.log(fib(28));
