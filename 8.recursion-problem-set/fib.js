7a01efd3cb834d1d0203ff365544c49b96975ec1
MinaMaherNicola <minamahernicola@outlook.com>
2021-02-14 17:53:44 +0200

    Solved fib -Without Recursion-

--- 8.recursion-problem-set/fib.js
+++ 8.recursion-problem-set/fib.js
@@ -3,18 +3,22 @@ const fib = (num) => {
     odd = 1;
   let res = -1;
 
-  num /= 2;
+  margin = Math.ceil(num / 2);
 
-  for (let i = 0; i < num; i++) {
+  for (let i = 0; i < margin; i++) {
     odd = odd + even;
     even = odd + even;
-    console.log(`Iteration: ${i} ||| Odd: ${odd}, Even: ${even}`);
 
-    if (num % 2 === 0) res = even;
-    else res = odd;
+    if ((i + 1) * 2 === num) {
+      res = even;
+    } else {
+      res = odd;
+    }
   }
-  console.log(num);
-  console.log(res);
+  return res;
 };
 
-fib(28);
+console.log(fib(4));
+console.log(fib(10));
+console.log(fib(28));
+console.log(fib(35));
