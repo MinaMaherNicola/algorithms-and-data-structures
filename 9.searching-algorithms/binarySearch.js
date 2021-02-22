3db1be13e3658ebc26dc0ebead0ee76dd2b00fae
mina <minamahernicola@outlook.com>
2021-02-23 01:03:27 +0200

    Fixed some issues to the binary search

--- 9.searching-algorithms/binarySearch.js
+++ 9.searching-algorithms/binarySearch.js
@@ -3,13 +3,11 @@ const binarySearch = (arr, val) => {
   let left = 0;
   let mid = Math.ceil((right - left) / 2);
 
-  for (let i = 0; i < 50; i++) {
-    console.log(`Right: ${right} | Left: ${left} | Mid: ${mid} | Mid Val: ${arr[mid]}`);
+  while (mid !== right) {
     mid = Math.ceil((right - left) / 2);
     mid += left;
     if (arr[mid] === val) {
       return mid;
-      break;
     } else if (val > arr[mid]) {
       left = mid + 1;
       continue;
@@ -23,4 +21,4 @@ const binarySearch = (arr, val) => {
 
 const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
-console.log(binarySearch(list, 16));
+console.log(binarySearch(list, 20));
