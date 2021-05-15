5f421869c3b975cc9a5415505866db5f5e7842fd
mina <minamahernicola@outlook.com>
2021-05-15 17:20:39 +0200

    Finished binary search in grokking

---
+++ grokking-algorithms/ch-1/README.md
@@ -0,0 +1,14 @@
+# In this chapter
+
+1. you get a foundation for the rest of the book
+2. You write your first search algorithm (binary search)
+3. You learn how to talk about the running time of an algorithm (Big O notation)
+4. You're intorudced to a common technique for designing algorithms (recursion)
+
+
+## Big O of Binary Search
+In general, for any sorted list of n, binary search will take log2n steps to run in the worst case, whereas simple search will take n steps.
+
+## Logarithms
+You may not remember what logarithms are, but you probably know what exponentials are. log10 100 is like asking, "How many 10s do we multiply together to get 100?" The answer is 2 (10 * 10) so log10 100 = 2.
+Logs are the flip of exponentials
---
+++ grokking-algorithms/ch-1/binarySearch.js
@@ -0,0 +1,46 @@
+/* 
+1. binarySearch function takes a sorted array and an item
+2. if the item is in the array, the function returns its position
+3. you will keep track of what part of the array you have to search through
+
+EXERCIES
+1.1 Suppose you have a sorted list of 128 items, and you're searching through it using binary search. What's the maximum number of steps it would take?
+- 7
+
+1.2 Suppose you double the size of the list, what's the maximum number of steps now?
+- 8
+*/
+
+const binarySearch = (arr = [], item) => {
+  let left = 0,
+    right = arr.length - 1,
+    mid = Math.floor(arr.length / 2);
+
+  while (mid !== arr.length) {
+    if (arr[mid] === item) {
+      return mid;
+    } else if (arr[mid] > item) {
+      right = mid - 1;
+      mid = Math.floor(right / 2);
+      continue;
+    } else if (arr[mid] < item) {
+      left = mid + 1;
+      mid = Math.floor((right + left) / 2);
+      continue;
+    }
+  }
+};
+
+const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(binarySearch(array, 0));
+console.log(binarySearch(array, 1));
+console.log(binarySearch(array, 2));
+console.log(binarySearch(array, 3));
+console.log(binarySearch(array, 4));
+console.log(binarySearch(array, 5));
+console.log(binarySearch(array, 6));
+console.log(binarySearch(array, 7));
+console.log(binarySearch(array, 8));
+console.log(binarySearch(array, 9));
+console.log(binarySearch(array, 10));
